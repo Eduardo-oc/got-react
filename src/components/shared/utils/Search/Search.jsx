f9a5bf30f53627191fc658bc23b4d55ed8e70c96
nkumar <nklullaregistros@hotmail.com>
2020-10-30 09:42:12 +0000

    traduccion boton y otras, cambio link homepage

--- src/components/shared/utils/Search/Search.jsx
+++ src/components/shared/utils/Search/Search.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
 import './Search.scss';
 
 export default function Search(props) {
   const { register, handleSubmit, errors } = useForm();
+  const { t, i18n } = useTranslation("global");
 
   const onKeyUp = (data) => {
     props.fnFilterCharacters(data);
@@ -22,7 +24,7 @@ export default function Search(props) {
         }
       }}
     >
-      <input className="row align-items-start search" type="text" name="name" id="name" placeholder="search" ref={register} />
+      <input className="row align-items-start search" type="text" name="name" id="name" placeholder={t("search.placeholder")} ref={register} />
     </form>
   );
 }
