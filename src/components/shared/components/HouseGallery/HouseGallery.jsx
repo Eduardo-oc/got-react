7740c74bae3e4c6c73e88498c85700c385bc547f
judith.12 <judithuelva_@hotmail.com>
2020-10-30 08:56:06 +0100

    filtro de imagenes para poner una por defecto y algo de estilos en el detalle

--- src/components/shared/components/HouseGallery/HouseGallery.jsx
+++ src/components/shared/components/HouseGallery/HouseGallery.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+//import './HouseGallery.scss'; 
 export default function HouseGallery(props) {
 
 
 
   return (
     <div className="container-fluid">
-    <div className="row">
+    <div className="d-flex flex-row flex-wrap justify-content-around">
       {props.houses.map((item, index) => 
-        <div >
-          <figure className='col-2'>
+        <div className="img" >
+          <figure className='col-2 size'>
           <Link to={'./HouseDetaills/'+ item.name}>
-            <img className="img" src={item.logoURL}></img>
+            <img src={item.logoURL ? item.logoURL:'https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/A_Song_of_Ice_and_Fire_arms_of_House_Lannister_red_scroll.png/220px-A_Song_of_Ice_and_Fire_arms_of_House_Lannister_red_scroll.png'}></img>
             <p>{item.name}</p>
             </Link>
           </figure>
