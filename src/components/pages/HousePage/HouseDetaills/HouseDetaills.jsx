2acb6a70f6533cc78e6d5666c404fb3b0633ba77
nkumar <nklullaregistros@hotmail.com>
2020-10-30 13:35:14 +0000

    cambios sobre merge Judit

--- src/components/pages/HousePage/HouseDetaills/HouseDetaills.jsx
+++ src/components/pages/HousePage/HouseDetaills/HouseDetaills.jsx
@@ -4,9 +4,14 @@ import { useParams } from "react-router-dom";
 import "./HouseDetails.scss";
 import SimpleBar from "simplebar-react";
 import "simplebar/dist/simplebar.css";
+import ButtonBack from "../../../shared/utils/ButtonBack/ButtonBack";
+import NavBar from "../../../shared/utils/NavBar/NavBar";
+import { useTranslation } from "react-i18next";
 
 export default function HouseDetaills() {
   const [houseName, setHouseName] = useState(useParams().name);
+
+  const { t, i18n } = useTranslation("global");
 
   useEffect(() => {
     axios
@@ -18,40 +23,46 @@ export default function HouseDetaills() {
 
   console.log(houseName);
   return (
-    <>
-      <div className="conteiner-fluid">
+    <div className="conteiner-fluid">
+      <div className="medium-box">
+        <div>
+          <ButtonBack />
+          <NavBar />
+        </div>
         <div className="d-flex flex-row justify-content-center text-center">
-          <figure className="col-1">
-            <img src={houseName[0].logoURL} alt=""></img>
-            <h1>{houseName[0].name}</h1>
+          <figure className="figure-detail col-6">
+            <img className="img-detail" src={houseName[0].logoURL} alt=""></img>
+            <figcaption className="figcaption-detail">
+              {houseName[0].name}
+            </figcaption>
           </figure>
         </div>
 
         <div className="row container">
           <div className="col-2 posicion">
-            <h4>LEMA</h4>
-            <p>{houseName[0].sigil}</p>
+            <h4>{t("houses.sigil")}</h4>
+            <p className="p-detail">{houseName[0].sigil}</p>
           </div>
 
           <div className="col-2 posicion">
-            <h4>SEDE</h4>
+            <h4>{t("houses.seat")}</h4>
             <SimpleBar style={{ maxHeight: 200 }}>
               {houseName &&
                 houseName[0].seat &&
                 houseName[0].seat.map((item) => {
-                  return <p>{item}</p>;
+                  return <p className="p-detail">{item}</p>;
                 })}
             </SimpleBar>
             {/* <p>{houseName[0].seat}</p>*/}
           </div>
 
           <div className="col-2 posicion">
-            <h4>REGION</h4>
+            <h4>{t("houses.region")}</h4>
             <SimpleBar style={{ maxHeight: 200 }}>
               {houseName &&
                 houseName[0].region &&
                 houseName[0].region.map((item) => {
-                  return <p>{item}</p>;
+                  return <p className="p-detail">{item}</p>;
                 })}
             </SimpleBar>
 
@@ -59,35 +70,34 @@ export default function HouseDetaills() {
           </div>
 
           <div className="col-2 posicion">
-            <h4>ALIANZAS</h4>
+            <h4>{t("houses.allegiances")}</h4>
             <SimpleBar style={{ maxHeight: 200 }}>
               {houseName &&
                 houseName[0].allegiance &&
                 houseName[0].allegiance.map((item) => {
-                  return <p>{item}</p>;
+                  return <p className="p-detail">{item}</p>;
                 })}
             </SimpleBar>
             {/*<p>{houseName[0].allegiance}</p>*/}
           </div>
 
           <div className="col-2 posicion">
-            <h4>RELIGIONES  </h4>
+            <h4>{t("houses.religion")} </h4>
             <SimpleBar style={{ maxHeight: 200 }}>
               {houseName &&
                 houseName[0].religion &&
                 houseName[0].religion.map((item) => {
-                  return <p>{item}</p>;
+                  return <p className="p-detail">{item}</p>;
                 })}
             </SimpleBar>
           </div>
 
           <div className="col-2 posicion">
-            <h4> FUNDACION</h4>
-            <p>{houseName[0].createdAt}</p>
+            <h4> {t("houses.foundation")}</h4>
+            <p className="p-detail">{houseName[0].createdAt}</p>
           </div>
-          
         </div>
       </div>
-    </>
+    </div>
   );
 }
